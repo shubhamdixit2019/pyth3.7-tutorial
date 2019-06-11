bdfdeba6aab9aca3b10e6ccdc25be3b22c870158
Shubham Dixit <sdixit@gammastack.com>
2019-06-11 20:23:12 +0530

    Dictionaries code added.

--- Basic/dictionaries.py
+++ Basic/dictionaries.py
@@ -1,3 +1,12 @@
 ''' 
-Used in a situation  when
+Used in a situation  when information needs to be stored as a key value pair,
+and key is expected to be unique
 '''
+customers = {
+    "name" : "Jon Snow",
+    "age" : 32,
+    "is_verified" : True
+}
+customers["birthdate"]= "27 Jan 2019"
+customers["name"]="Lord Varys"
+print(customers["name"])
